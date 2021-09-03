291de15d80f301d28e2c6f2e7f71c7294c7a7fca
webii <webii@pm.me>
2021-09-03 18:33:44 +0400

    Init templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
+from difflib import HtmlDiff
+
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Note, History
+
+
+def index(request):
+    notes = Note.objects.filter()
+    return render(request, "index.html", {"notes": notes})
+
+
+def page(request, slug):
+    note = get_object_404(Note, slug=slug)
+    return render(request, "page.html", {"note": note})
+
+
+def page_history(request, slug):
+    note = get_object_404(Note, slug=slug)
+    history_list = History.objects.filter(note=note).order_by("-added")
+    return render(request, "page-history.html", {"history": history})
